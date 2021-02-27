e90d65f2f136a7f2895f97256a60d6e9c3655ea6
Kevin Charles <kevinismage@gmail.com>
2021-02-28 06:37:13 +0700

    Login page Finish

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,19 @@ import Schedule from './pages/Schedule';
 import Organization from './pages/Organization';
 import NotFound from './pages/NotFound';
 import Admin from './pages/Admin';
+import Authentication from './pages/Authentication';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
+      <Navbar />
       <Router>
         <Switch>
           <Route exact path='/'>
             <Index />
+          </Route>
+          <Route exact path='/login'>
+            <Authentication />
           </Route>
           <Route exact path='/schedule'>
             <Schedule />
